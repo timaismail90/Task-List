fdf5025f0e2a1c3b3a05f2bb219565bd7258d484
Fatuma Ismail <timaismail90gmail.com>
2020-01-21 21:27:49 -0600

    added a function to store tasks in local storage

--- app.js
+++ app.js
@@ -51,14 +51,26 @@ function addTask(e) {
     // console.log(li);
     taskList.appendChild(li);
 
+    // Store in local storage;
+    storeTaskInLocalStorage(taskInput.value);
+
     //clear input
     taskInput.value = '';
 
-    
-
-
     e.preventDefault();
 
+}
+// store task
+function storeTaskInLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // RemoveTask function 
